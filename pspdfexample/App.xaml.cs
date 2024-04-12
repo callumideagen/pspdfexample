a0a105a357615c21d4c7ac5ed8af3bc18a74dbe8
Callum Brown <callum.brown@ideagenplc.com>
2024-04-12 08:43:35 +0100

    pspdf poc created

---
+++ pspdfexample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace pspdfexample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ pspdfexample/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using PSPDFKit.Sdk;
+
+namespace pspdfexample;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void OnPDFViewInitialized(object sender, EventArgs e)
+    {
+        try
+        {
+            await PDFView.Controller.LoadDocumentFromAssetsAsync("test.docx", null);
+        }
+        catch (Exception ex)
+        {
+            // Handle exception.
+        }
+    }
+}
+
+
